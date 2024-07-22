9bab0208d1bcce263b4dba10906c23a9312ae811
SkylarHoughtonIntelerad <Skylar.Houghton@Intelerad.com>
2024-07-22 16:34:22 -0500

    dir

--- test/tf_test.go
+++ test/tf_test.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"os"
 	"os/exec"
 	"testing"
 )
 
+var dir string
+
 // Helper function to run a command and capture its output
-func runCommand(t *testing.T, name string, args ...string) (string, error) {
+func runCommand(t *testing.T, dir string, name string, args ...string) (string, error) {
 	cmd := exec.Command(name, args...)
+	cmd.Dir = dir
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &out
@@ -17,8 +22,7 @@ func runCommand(t *testing.T, name string, args ...string) (string, error) {
 }
 
 func TestTerraformInit(t *testing.T) {
-	
-	output, err := runCommand(t, "terraform", "init", "-input=false")
+	output, err := runCommand(t, dir, "terraform", "init", "-input=false")
 	if err != nil {
 		t.Fatalf("terraform init failed: %v\nOutput: %s", err, output)
 	}
@@ -26,10 +30,23 @@ func TestTerraformInit(t *testing.T) {
 }
 
 func TestTerraformValidate(t *testing.T) {
-
-	output, err := runCommand(t, "terraform", "validate")
+	output, err := runCommand(t, dir, "terraform", "validate")
 	if err != nil {
 		t.Fatalf("terraform validate failed: %v\nOutput: %s", err, output)
 	}
 	t.Logf("terraform validate succeeded\nOutput: %s", output)
+}
+
+func TestMain(m *testing.M) {
+	// Set the directory where the Terraform configuration files are located
+	flag.StringVar(&dir, "dir", ".", "directory containing Terraform configuration files")
+	flag.Parse()
+
+	if dir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Run the tests
+	os.Exit(m.Run())
 }
