1d853950c9397e06d9485f35b89b8b4b17e6c6dd
SkylarHoughtonIntelerad <Skylar.Houghton@Intelerad.com>
2024-07-22 16:39:44 -0500

    test#

--- environment/prod/arsc/aws-ssm-patch/patch_group.tf
+++ environment/prod/arsc/aws-ssm-patch/patch_group.tf
@@ -1,42 +1,42 @@
-module "testgroup-patchgroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
-  patch_group = local.testgroup_patch_name
-  baseline_id = var.baseline_id
-  depends_on  = [module.testgroup-patch-window]
-}
+# module "testgroup-patchgroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
+#   patch_group = local.testgroup_patch_name
+#   baseline_id = var.baseline_id
+#   depends_on  = [module.testgroup-patch-window]
+# }
 
-module "testgroup-resourcegroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
-  name        = local.testgroup_patch_name
-  description = var.description
-  tags        = var.testgroup_tags
-}
+# module "testgroup-resourcegroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
+#   name        = local.testgroup_patch_name
+#   description = var.description
+#   tags        = var.testgroup_tags
+# }
 
-module "group1-resourcegroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
-  name        = local.group1_patch_name
-  description = var.description
-  tags        = var.group1_tags
-}
+# module "group1-resourcegroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
+#   name        = local.group1_patch_name
+#   description = var.description
+#   tags        = var.group1_tags
+# }
 
-module "group1-patchgroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
-  patch_group = local.group1_patch_name
-  #baseline_id = module.windows-pb.windows-pb-arn #Module call for custom baselines
-  baseline_id = var.baseline_id
-  depends_on  = [module.group1-patch-window]
-}
+# module "group1-patchgroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
+#   patch_group = local.group1_patch_name
+#   #baseline_id = module.windows-pb.windows-pb-arn #Module call for custom baselines
+#   baseline_id = var.baseline_id
+#   depends_on  = [module.group1-patch-window]
+# }
 
-module "group2-resourcegroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
-  name        = local.group2_patch_name
-  description = var.description
-  tags        = var.group2_tags
-}
+# module "group2-resourcegroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/resource-group-patch-groups?ref=master"
+#   name        = local.group2_patch_name
+#   description = var.description
+#   tags        = var.group2_tags
+# }
 
-module "group2-patchgroup" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
-  patch_group = local.group2_patch_name
-  baseline_id = var.baseline_id
-  depends_on  = [module.group2-patch-window]
-}
+# module "group2-patchgroup" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-patch-group?ref=master"
+#   patch_group = local.group2_patch_name
+#   baseline_id = var.baseline_id
+#   depends_on  = [module.group2-patch-window]
+# }
--- environment/prod/arsc/aws-ssm-patch/targets.tf
+++ environment/prod/arsc/aws-ssm-patch/targets.tf
@@ -1,62 +1,62 @@
-module "testgroup-patch-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.testgroup_patch_name
-  description = local.testgroup_patch_name
-  window_id   = module.testgroup-patch-window.maintenance-window-id
-  depends_on  = [module.testgroup-patch-window]
-}
-module "testgroup-reboot-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.testgroup_reboot_name
-  description = local.testgroup_reboot_name
-  window_id   = module.testgroup-reboot-window.maintenance-window-id
-  depends_on  = [module.testgroup-reboot-window]
-}
+# module "testgroup-patch-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.testgroup_patch_name
+#   description = local.testgroup_patch_name
+#   window_id   = module.testgroup-patch-window.maintenance-window-id
+#   depends_on  = [module.testgroup-patch-window]
+# }
+# module "testgroup-reboot-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.testgroup_reboot_name
+#   description = local.testgroup_reboot_name
+#   window_id   = module.testgroup-reboot-window.maintenance-window-id
+#   depends_on  = [module.testgroup-reboot-window]
+# }
 
-module "group1-evac-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group2_patch_name
-  description = local.group2_patch_name
-  window_id   = module.group1-reboot-window.maintenance-window-id #Schedule group2 resources in group1 window for failover
-  depends_on  = [module.group1-reboot-window]
-}
+# module "group1-evac-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group2_patch_name
+#   description = local.group2_patch_name
+#   window_id   = module.group1-reboot-window.maintenance-window-id #Schedule group2 resources in group1 window for failover
+#   depends_on  = [module.group1-reboot-window]
+# }
 
-module "group2-evac-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group1_patch_name
-  description = local.group1_patch_name
-  window_id   = module.group2-reboot-window.maintenance-window-id #Schedule group1 resources in group2 window for failover
-  depends_on  = [module.group2-reboot-window]
-}
+# module "group2-evac-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group1_patch_name
+#   description = local.group1_patch_name
+#   window_id   = module.group2-reboot-window.maintenance-window-id #Schedule group1 resources in group2 window for failover
+#   depends_on  = [module.group2-reboot-window]
+# }
 
-module "group1-patch-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group1_patch_name
-  description = local.group1_patch_name
-  window_id   = module.group1-patch-window.maintenance-window-id
-  depends_on  = [module.group1-patch-window]
-}
+# module "group1-patch-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group1_patch_name
+#   description = local.group1_patch_name
+#   window_id   = module.group1-patch-window.maintenance-window-id
+#   depends_on  = [module.group1-patch-window]
+# }
 
-module "group2-patch-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group2_patch_name
-  description = local.group2_patch_name
-  window_id   = module.group2-patch-window.maintenance-window-id
-  depends_on  = [module.group2-patch-window]
-}
+# module "group2-patch-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group2_patch_name
+#   description = local.group2_patch_name
+#   window_id   = module.group2-patch-window.maintenance-window-id
+#   depends_on  = [module.group2-patch-window]
+# }
 
-module "group1-reboot-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group1_reboot_name
-  description = local.group1_reboot_name
-  window_id   = module.group1-reboot-window.maintenance-window-id
-  depends_on  = [module.group1-reboot-window]
-}
+# module "group1-reboot-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group1_reboot_name
+#   description = local.group1_reboot_name
+#   window_id   = module.group1-reboot-window.maintenance-window-id
+#   depends_on  = [module.group1-reboot-window]
+# }
 
-module "group2-reboot-target" {
-  source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
-  name        = local.group2_reboot_name
-  description = local.group2_reboot_name
-  window_id   = module.group2-reboot-window.maintenance-window-id
-  depends_on  = [module.group2-reboot-window]
-}
+# module "group2-reboot-target" {
+#   source      = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/ssm-target?ref=master"
+#   name        = local.group2_reboot_name
+#   description = local.group2_reboot_name
+#   window_id   = module.group2-reboot-window.maintenance-window-id
+#   depends_on  = [module.group2-reboot-window]
+# }
