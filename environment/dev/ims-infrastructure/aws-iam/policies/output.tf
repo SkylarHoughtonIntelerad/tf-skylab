5f545a2dc48b915944263126b9d0f0dc3ff5e96d
SkylarHoughtonIntelerad <Skylar.Houghton@Intelerad.com>
2024-07-23 13:49:17 -0500

    removed noisy

--- environment/dev/ims-infrastructure/aws-iam/policies/output.tf
+++
@@ -1,2 +0,0 @@
-output "policydoc_cw_logs_to_s3" { value = data.aws_iam_policy_document.policydoc_cw_logs_to_s3.json }
-output "policydoc_slackbot" { value = data.aws_iam_policy_document.policydoc_slackbot.json }
